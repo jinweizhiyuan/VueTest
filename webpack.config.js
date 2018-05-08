1a13ad0f18a2fb9f753535ba0a8bd70c88aa5df0
hqlip <hqlip>
2018-05-08 17:51:59 +0800

    append extract text

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const HTMLPlugin = require('html-webpack-plugin');
 const webpack = require('webpack');
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
 
 const isDev = process.env.NODE_ENV == 'development';
 
@@ -9,15 +10,15 @@ var config = {
     mode: 'development',
     entry: path.join(__dirname, 'src/index.js'),
     output: {
-        filename: 'bundle.js',
+        // filename: 'bundle.js',
+        filename: 'bundle.[hash:8].js',
         path: path.join(__dirname, "dist")
     },
     // resolveLoader: {
     //     root: path.join(__dirname, 'node_modules')
     // },
     module: {
-        rules:[
-            {
+        rules: [{
                 test: /\.vue$/,
                 loader: 'vue-loader'
             },
@@ -25,27 +26,27 @@ var config = {
                 test: /\.jsx$/,
                 loader: 'babel-loader'
             },
-            {
-                test: /\.css$/,
-                use: [
-                    'style-loader',
-                    'css-loader'
-                ]
-            },
-            {
-                test: /\.styl$/,
-                use: [
-                    'style-loader',
-                    'css-loader',
-                    {
-                        loader: 'postcss-loader',
-                        options: {
-                            sourceMap: true
-                        }
-                    },
-                    'stylus-loader'
-                ]
-            },
+            // {
+            //     test: /\.css$/,
+            //     use: [
+            //         'style-loader',
+            //         'css-loader'
+            //     ]
+            // },
+            // {
+            //     test: /\.styl$/,
+            //     use: [
+            //         'style-loader',
+            //         'css-loader',
+            //         {
+            //             loader: 'postcss-loader',
+            //             options: {
+            //                 sourceMap: true
+            //             }
+            //         },
+            //         'stylus-loader'
+            //     ]
+            // },
             {
                 test: /\.(jpg|jpeg|png|svg|gif)$/,
                 use: {
@@ -68,7 +69,21 @@ var config = {
     ]
 };
 
-if  (isDev) {
+if (isDev) {
+    config.module.rules.push({
+        test: /\.styl$/,
+        use: [
+            'style-loader',
+            'css-loader',
+            {
+                loader: 'postcss-loader',
+                options: {
+                    sourceMap: true
+                }
+            },
+            'stylus-loader'
+        ]
+    });
     config.devtool = '#cheap-eval-resource-map';
     config.devServer = {
         port: 8000,
@@ -84,6 +99,27 @@ if  (isDev) {
     config.plugins.push(
         new webpack.HotModuleReplacementPlugin(),
         new webpack.NoEmitOnErrorsPlugin()
+    )
+} else {
+    config.output.filename = '[name].[chunkhash:8].js'
+    config.module.rules.push({
+        test: /\.styl$/,
+        use: ExtractTextPlugin.extract({
+            fallback: 'style-loader',
+            use: [
+                'css-loader',
+                {
+                    loader: 'postcss-loader',
+                    options: {
+                        sourceMap: true
+                    }
+                },
+                'stylus-loader'
+            ]
+        })
+    });
+    config.plugins.push(
+        new ExtractTextPlugin('styles.[md5:contenthash:hex:8].css')
     )
 }
 
